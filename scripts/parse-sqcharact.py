af15f30523e9b1de74c024cc54b8bb2ed2c30244
mariojmdavid <mariojmdavid@gmail.com>
2023-08-03 12:03:39 +0100

    implementing comments

--- scripts/parse-sqcharact.py
+++ scripts/parse-sqcharact.py
@@ -14,7 +14,10 @@ if __name__ == '__main__':
     with open(csv_file, encoding='utf-8', newline='') as csvf:
         reader = csv.DictReader(csvf)
         for row in reader:
-            latex_str += f'\\textbf{{{row["Characteristic"]}}}: {row["Definition"]} \\textbf{{Notes}}: {row["Notes"]}\n\n'
+            if row["Notes"] == '-':
+                latex_str += f'\\textbf{{{row["Characteristic"]}}}: {row["Definition"]}\n\n'
+            else:
+                latex_str += f'\\textbf{{{row["Characteristic"]}}}: {row["Definition"]} \\textbf{{Notes}}: {row["Notes"]}\n\n'
 
     print(latex_str)
     with open('../latex/qual-char.tex', 'w', encoding='utf-8') as ftex:
